79be39aa63b05af73afe3a8241b1c4a62b3a0771
Kiyanti <vladimirsverenyak@gmail.com>
2024-05-21 18:54:32 +0300

    Basic layout done

---
+++ src/components/Home.jsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import Section from "./Section";
+
+export default function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        backgroundImg="../public/model-s.jpg"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        backgroundImg="../public/model-y.jpg"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        backgroundImg="../public/model-3.jpg"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        backgroundImg="../public/model-x.jpg"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        backgroundImg="../public/solar-panel.jpg"
+        description="Money-back guarantee"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Solar for New Roofs"
+        backgroundImg="../public/solar-roof.jpg"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Accesories"
+        backgroundImg="../public/accessories.jpg"
+        description=""
+        leftBtnText="Shop now"
+      />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+`;
---
+++ src/components/Section.jsx
@@ -0,0 +1,96 @@
+import styled, { keyframes } from "styled-components";
+
+export default function Section({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) {
+  return (
+    <Wrapper backgroundImg={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="../public/down-arrow.svg" />
+      </Buttons>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url(${({ backgroundImg }) => backgroundImg});
+  /* background-image: url(${(props) => props.backgroundImg}); */
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.button`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+`;
+
+const animateDown = keyframes`
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateY(0);
+  }
+
+  40% {
+    transform: translateY(5px);
+  }
+
+  60% {
+    transform: translateY(3px);
+  }
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: ${animateDown} infinite 1.5s;
+`;
+
+const Buttons = styled.div``;
